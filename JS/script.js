d19eb789ede89949d6379aaf65caac97c536eb0e
Kacper <krzysztofzajac975@gmail.com>
2024-02-09 20:24:23 +0100

    not a final version of nav and header

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,7 @@
 const navbarMobileLogo = document.querySelector(
 	'.nav__mobile .nav__item--logo img'
 );
+const header = document.querySelector('.header');
 const headerImage = document.querySelector('.header__carousel-photo--photo1');
 let i = 1;
 
@@ -14,19 +15,22 @@ const showAndHidePhotos = () => {
 	setTimeout(() => {
 		headerImage.style.backgroundImage =
 			"url('" + `../img/carousel-item${i}-big.jpg')`;
-	}, 1200);
+	}, 1300);
 	setTimeout(() => {
 		headerImage.classList.toggle('show');
 		headerImage.classList.toggle('hide');
-	}, 1200);
+	}, 1400);
 	setTimeout(showAndHidePhotos, 5000);
 };
-setTimeout(showAndHidePhotos, 5000);
-
-document.body.addEventListener('click', () => {
+const checkScreenWidth = () => {
 	if (window.innerWidth >= 768) {
 		navbarMobileLogo.setAttribute('src', '../img/logo.png');
+		header.style.height = 'calc(100vh - 120px)';
 	} else {
 		navbarMobileLogo.setAttribute('src', '../img/logo-smaller.png');
+		header.style.height = 'calc(100vh - 100px)';
 	}
-});
+};
+setTimeout(showAndHidePhotos, 5000);
+document.addEventListener('DOMContentLoaded', checkScreenWidth);
+window.addEventListener('resize', checkScreenWidth);
