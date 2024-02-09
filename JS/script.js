064835c9c6a8fee6b5c75b209c98d57849c26350
Kacper <krzysztofzajac975@gmail.com>
2024-02-09 19:44:56 +0100

    added header section and many header photos fade effect improvements

--- JS/script.js
+++ JS/script.js
@@ -2,25 +2,31 @@ const navbarMobileLogo = document.querySelector(
 	'.nav__mobile .nav__item--logo img'
 );
 const headerImage = document.querySelector('.header__carousel-photo--photo1');
+let i = 1;
+
+const showAndHidePhotos = () => {
+	headerImage.classList.toggle('show');
+	headerImage.classList.toggle('hide');
+	if (i == 3) {
+		i = 0;
+	}
+	i++;
+	setTimeout(() => {
+		headerImage.style.backgroundImage =
+			"url('" + `../img/carousel-item${i}-big.jpg')`;
+	}, 1200);
+	setTimeout(() => {
+		headerImage.classList.toggle('show');
+		headerImage.classList.toggle('hide');
+	}, 1200);
+	setTimeout(showAndHidePhotos, 5000);
+};
+setTimeout(showAndHidePhotos, 5000);
+
 document.body.addEventListener('click', () => {
 	if (window.innerWidth >= 768) {
 		navbarMobileLogo.setAttribute('src', '../img/logo.png');
 	} else {
 		navbarMobileLogo.setAttribute('src', '../img/logo-smaller.png');
 	}
-});
-document.addEventListener('DOMContentLoaded', () => {
-	let i = 1;
-	setInterval(() => {
-		headerImage.classList.toggle('show');
-		headerImage.classList.toggle('hide');
-	}, 3500);
-	setInterval(() => {
-		i++;
-		headerImage.style.backgroundImage =
-			"url('" + `../img/carousel-item${i}-big.jpg')`;
-		if (i == 3) {
-			i = 0;
-		}
-	}, 7000);
 });
