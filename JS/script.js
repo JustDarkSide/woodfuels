ed65d083783ddeadd2108cf4ff0733a67f307953
Kacper <krzysztofzajac975@gmail.com>
2024-02-11 18:38:35 +0100

    working shadow finally!

--- JS/script.js
+++ JS/script.js
@@ -1,31 +1,57 @@
-const mbNav = document.querySelector(".mobile-nav");
-const bBtn = document.querySelector(".burger-btn");
-const allNavItems = document.querySelectorAll(".mb-nav__item");
-const header = document.querySelector(".header");
-const headerImage = document.querySelector(".header__carousel-photo--photo1");
-const navSpace = document.querySelector(".nav");
-const offsetDiv = document.querySelector(".offset");
+const mbNav = document.querySelector('.mobile-nav');
+const bBtn = document.querySelector('.burger-btn');
+const allNavItems = document.querySelectorAll('.mb-nav__item');
+const header = document.querySelector('.header');
+const headerImage = document.querySelector('.header__carousel-photo--photo1');
+const navSpace = document.querySelector('.nav');
+const offsetDiv = document.querySelector('.offset');
 let i = 1;
 
-navSpace.addEventListener("focusout", () => {
+navSpace.addEventListener('focusout', () => {
 	//Kacperek zabłysnął
-	mbNav.classList.remove("mobile-nav--active");
-	offsetDiv.style.opacity = 0;
+	mbNav.classList.remove('mobile-nav--active');
+	setTimeout(() => {
+		if (mbNav.classList.contains('mobile-nav--active')) {
+			offsetDiv.classList.add('showShadow');
+			offsetDiv.classList.remove('hideShadow');
+		} else {
+			offsetDiv.classList.remove('showShadow');
+			offsetDiv.classList.add('hideShadow');
+		}
+	}, 50);
+	//to zrobił dominik elegancko nie
 });
-
 const handleNav = () => {
-	mbNav.classList.toggle("mobile-nav--active");
+	mbNav.classList.toggle('mobile-nav--active');
+	setTimeout(() => {
+		if (mbNav.classList.contains('mobile-nav--active')) {
+			offsetDiv.classList.add('showShadow');
+			offsetDiv.classList.remove('hideShadow');
+		} else {
+			offsetDiv.classList.remove('showShadow');
+			offsetDiv.classList.add('hideShadow');
+		}
+	}, 50);
 
 	allNavItems.forEach((item) => {
-		item.addEventListener("click", () => {
-			mbNav.classList.remove("mobile-nav--active");
+		item.addEventListener('click', () => {
+			mbNav.classList.remove('mobile-nav--active');
+			setTimeout(() => {
+				if (mbNav.classList.contains('mobile-nav--active')) {
+					offsetDiv.classList.add('showShadow');
+					offsetDiv.classList.remove('hideShadow');
+				} else {
+					offsetDiv.classList.remove('showShadow');
+					offsetDiv.classList.add('hideShadow');
+				}
+			}, 50);
 		});
 	});
 };
 
 const showAndHidePhotos = () => {
-	headerImage.classList.toggle("show");
-	headerImage.classList.toggle("hide");
+	headerImage.classList.toggle('show');
+	headerImage.classList.toggle('hide');
 	if (i == 3) {
 		i = 0;
 	}
@@ -35,19 +61,19 @@ const showAndHidePhotos = () => {
 			"url('" + `../img/carousel-item${i}-big.jpg')`;
 	}, 1300);
 	setTimeout(() => {
-		headerImage.classList.toggle("show");
-		headerImage.classList.toggle("hide");
+		headerImage.classList.toggle('show');
+		headerImage.classList.toggle('hide');
 	}, 1400);
 	setTimeout(showAndHidePhotos, 5000);
 };
 const checkScreenWidth = () => {
 	if (window.innerWidth >= 768) {
-		header.style.height = "calc(100vh - 120px)";
+		header.style.height = 'calc(100vh - 120px)';
 	} else {
-		header.style.height = "calc(100vh - 90px)";
+		header.style.height = 'calc(100vh - 90px)';
 	}
 };
 setTimeout(showAndHidePhotos, 5000);
-document.addEventListener("DOMContentLoaded", checkScreenWidth);
-window.addEventListener("resize", checkScreenWidth);
-bBtn.addEventListener("click", handleNav);
+document.addEventListener('DOMContentLoaded', checkScreenWidth);
+window.addEventListener('resize', checkScreenWidth);
+bBtn.addEventListener('click', handleNav);
