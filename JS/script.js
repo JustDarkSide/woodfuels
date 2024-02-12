87155403d4233987769a6a078441ae295edaec9a
Kacper <krzysztofzajac975@gmail.com>
2024-02-12 20:25:55 +0100

    image positions changed

--- JS/script.js
+++ JS/script.js
@@ -1,33 +1,33 @@
-const mbNav = document.querySelector(".mobile-nav");
-const bBtn = document.querySelector(".burger-btn");
-const allNavItems = document.querySelectorAll(".mb-nav__item");
-const header = document.querySelector(".header");
-const headerImage = document.querySelector(".header__carousel-photo--photo1");
-const navSpace = document.querySelector(".nav");
-const offsetDiv = document.querySelector(".offset");
-const logoImg = document.querySelector(".solo-logo-item-img");
+const mbNav = document.querySelector('.mobile-nav');
+const bBtn = document.querySelector('.burger-btn');
+const allNavItems = document.querySelectorAll('.mb-nav__item');
+const header = document.querySelector('.header');
+const headerImage = document.querySelector('.header__carousel-photo--photo1');
+const navSpace = document.querySelector('.nav');
+const offsetDiv = document.querySelector('.offset');
+const logoImg = document.querySelector('.solo-logo-item-img');
 let i = 1;
 
 //-------------NAV--------------
 const offsetTimeout = () => {
 	setTimeout(() => {
-		if (mbNav.classList.contains("mobile-nav--active")) {
-			offsetDiv.classList.add("showShadow");
-			offsetDiv.classList.remove("hideShadow");
+		if (mbNav.classList.contains('mobile-nav--active')) {
+			offsetDiv.classList.add('showShadow');
+			offsetDiv.classList.remove('hideShadow');
 		} else {
-			offsetDiv.classList.remove("showShadow");
-			offsetDiv.classList.add("hideShadow");
+			offsetDiv.classList.remove('showShadow');
+			offsetDiv.classList.add('hideShadow');
 		}
 	}, 50);
 };
 
 const handleNav = () => {
-	mbNav.classList.toggle("mobile-nav--active");
+	mbNav.classList.toggle('mobile-nav--active');
 	offsetTimeout();
 
 	allNavItems.forEach((item) => {
-		item.addEventListener("click", () => {
-			mbNav.classList.remove("mobile-nav--active");
+		item.addEventListener('click', () => {
+			mbNav.classList.remove('mobile-nav--active');
 			offsetTimeout();
 		});
 	});
@@ -35,8 +35,8 @@ const handleNav = () => {
 
 //-------------HEADER-----------------
 const showAndHidePhotos = () => {
-	headerImage.classList.toggle("show");
-	headerImage.classList.toggle("hide");
+	headerImage.classList.toggle('show');
+	headerImage.classList.toggle('hide');
 	if (i == 5) {
 		i = 0;
 	}
@@ -45,47 +45,45 @@ const showAndHidePhotos = () => {
 		headerImage.style.backgroundImage =
 			"url('" + `../img/carousel-item${i}-big.jpg')`;
 		if (window.innerWidth <= 768 && i == 2) {
-			headerImage.style.backgroundPosition = "-120px 0";
+			headerImage.style.backgroundPosition = '0 0';
 		} else if (window.innerWidth >= 1200 && i == 2) {
-			headerImage.style.backgroundPosition = "0 -110px";
+			headerImage.style.backgroundPosition = '0 -110px';
 		} else if (window.innerWidth >= 1200 && i == 4) {
-			headerImage.style.backgroundPosition = "0 -300px";
+			headerImage.style.backgroundPosition = '0 -300px';
 		} else if (window.innerWidth >= 1200 && i == 5) {
-			headerImage.style.backgroundPosition = "0 -200px";
-		} else if (window.innerWidth <= 768 && i == 4) {
-			headerImage.style.backgroundPosition = "-300px 0";
+			headerImage.style.backgroundPosition = '0 -200px';
 		} else {
-			headerImage.style.backgroundPosition = "0 0";
+			headerImage.style.backgroundPosition = '0 0';
 		}
 	}, 1300);
 	setTimeout(() => {
-		headerImage.classList.toggle("show");
-		headerImage.classList.toggle("hide");
+		headerImage.classList.toggle('show');
+		headerImage.classList.toggle('hide');
 	}, 1400);
 	setTimeout(showAndHidePhotos, 5000);
 };
 
 const checkScreenWidth = () => {
 	if (window.innerWidth >= 768) {
-		header.style.height = "calc(60vh - 120px)";
+		header.style.height = 'calc(60vh - 120px)';
 	} else {
-		header.style.height = "calc(60vh - 90px)";
+		header.style.height = 'calc(60vh - 90px)';
 	}
 
 	if (window.innerWidth <= 400) {
-		logoImg.setAttribute("src", "../img/logo-circle-pc-apr.png");
+		logoImg.setAttribute('src', '../img/logo-circle-pc-apr.png');
 	}
 };
 
 setTimeout(showAndHidePhotos, 5000);
 
 //--------------EVENTS----------------
-document.addEventListener("DOMContentLoaded", checkScreenWidth);
-window.addEventListener("resize", checkScreenWidth);
-bBtn.addEventListener("click", handleNav);
-navSpace.addEventListener("focusout", () => {
+document.addEventListener('DOMContentLoaded', checkScreenWidth);
+window.addEventListener('resize', checkScreenWidth);
+bBtn.addEventListener('click', handleNav);
+navSpace.addEventListener('focusout', () => {
 	//Kacperek zabłysnął
-	mbNav.classList.remove("mobile-nav--active");
+	mbNav.classList.remove('mobile-nav--active');
 	offsetTimeout();
 	//to zrobił dominik elegancko nie
 });
