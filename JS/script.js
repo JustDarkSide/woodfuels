da04a3689f64b330577d97aa341b8f1605836771
Kacper <krzysztofzajac975@gmail.com>
2024-02-19 15:20:56 +0100

    little changes and script fixed

--- JS/script.js
+++ JS/script.js
@@ -1,12 +1,12 @@
-const mbNav = document.querySelector(".mobile-nav");
-const bBtn = document.querySelector(".burger-btn");
-const allNavItems = document.querySelectorAll(".mb-nav__item");
-const header = document.querySelector(".header");
-const headerImage = document.querySelector(".header__carousel-photo--photo1");
-const navSpace = document.querySelector(".nav");
-const offsetDiv = document.querySelector(".offset");
-const logoImg = document.querySelector(".solo-logo-item-img");
-const cardsTextSections = document.querySelectorAll(".cards__item-text");
+const mbNav = document.querySelector('.mobile-nav');
+const bBtn = document.querySelector('.burger-btn');
+const allNavItems = document.querySelectorAll('.mb-nav__item');
+const header = document.querySelector('.header');
+const headerImage = document.querySelector('.header__carousel-photo--photo1');
+const navSpace = document.querySelector('.nav');
+const offsetDiv = document.querySelector('.offset');
+const logoImg = document.querySelector('.solo-logo-item-img');
+const cardsTextSections = document.querySelectorAll('.cards__item-text');
 const navHeightSmall = 90;
 const navHeightMediumAndUnusual = 120;
 const carouselSectionHeightSmall = (window.innerHeight - navHeightSmall) * 0.6;
@@ -22,25 +22,25 @@ let y = 0;
 //-------------NAV--------------
 const offsetTimeout = () => {
 	setTimeout(() => {
-		if (mbNav.classList.contains("mobile-nav--active")) {
-			offsetDiv.classList.add("showShadow");
-			offsetDiv.classList.remove("hideShadow");
+		if (mbNav.classList.contains('mobile-nav--active')) {
+			offsetDiv.classList.add('showShadow');
+			offsetDiv.classList.remove('hideShadow');
 		} else {
-			offsetDiv.classList.remove("showShadow");
-			offsetDiv.classList.add("hideShadow");
+			offsetDiv.classList.remove('showShadow');
+			offsetDiv.classList.add('hideShadow');
 		}
 	}, 50);
 };
 
 const handleNav = () => {
-	document.body.classList.toggle("mobileMenuLock");
-	mbNav.classList.toggle("mobile-nav--active");
+	document.body.classList.toggle('mobileMenuLock');
+	mbNav.classList.toggle('mobile-nav--active');
 	offsetTimeout();
 
 	allNavItems.forEach((item) => {
-		item.addEventListener("click", () => {
-			document.body.classList.toggle("mobileMenuLock");
-			mbNav.classList.remove("mobile-nav--active");
+		item.addEventListener('click', () => {
+			document.body.classList.toggle('mobileMenuLock');
+			mbNav.classList.remove('mobile-nav--active');
 			offsetTimeout();
 		});
 	});
@@ -48,8 +48,8 @@ const handleNav = () => {
 
 //-------------HEADER-----------------
 const showAndHidePhotos = () => {
-	headerImage.classList.toggle("show");
-	headerImage.classList.toggle("hide");
+	headerImage.classList.toggle('show');
+	headerImage.classList.toggle('hide');
 	if (i == 5) {
 		i = 0;
 	}
@@ -75,73 +75,70 @@ const showAndHidePhotos = () => {
 		// }
 	}, 1300);
 	setTimeout(() => {
-		headerImage.classList.toggle("show");
-		headerImage.classList.toggle("hide");
+		headerImage.classList.toggle('show');
+		headerImage.classList.toggle('hide');
 	}, 1400);
 	setTimeout(showAndHidePhotos, 5000);
 };
 
 const checkScreenWidth = () => {
 	if (window.innerWidth >= 768) {
-		header.style.height = "calc(60vh - 120px)";
+		header.style.height = 'calc(60vh - 120px)';
 	} else {
-		header.style.height = "calc(60vh - 90px)";
+		header.style.height = 'calc(60vh - 90px)';
 	}
 
 	if (window.innerWidth <= 400) {
-		logoImg.setAttribute("src", "../img/logo-circle-pc-apr.png");
+		logoImg.setAttribute('src', '../img/logo-circle-pc-apr.png');
 	}
 };
 
 const showCardsDescription = () => {
-	for (const cardItem of cardsTextSections) {
-		if (cardItem.style.top != "150%") {
-			if (
-				window.scrollY >= offerSectionOffsetSmall &&
-				window.innerWidth < 768
-			) {
-				cardsTextSections[y].classList.add("showCardsText");
-				y++;
-				offerSectionOffsetSmall += 400;
-			} else if (
-				window.scrollY >= offerSectionOffsetMediumAndUnusual &&
-				window.innerWidth >= 768 &&
-				window.innerWidth < 1200
-			) {
-				cardsTextSections[y].classList.add("showCardsText");
-				y++;
-				if (y == 2) {
-					offerSectionOffsetMediumAndUnusual += 500;
-				}
-			} else if (
-				window.scrollY >= offerSectionOffsetMediumAndUnusual &&
-				window.innerWidth >= 1000 &&
-				window.innerWidth < 1200
-			) {
-				cardsTextSections[y].classList.add("showCardsText");
-				y++;
-				if (y == 2) {
-					offerSectionOffsetMediumAndUnusual += 550;
-				}
-			}
+	if (y == 4) {
+		return;
+	}
+	if (window.scrollY >= offerSectionOffsetSmall && window.innerWidth < 768) {
+		cardsTextSections[y].classList.add('showCardsText');
+		y++;
+		offerSectionOffsetSmall += 400;
+	} else if (
+		window.scrollY >= offerSectionOffsetMediumAndUnusual &&
+		window.innerWidth >= 768 &&
+		window.innerWidth < 1200
+	) {
+		cardsTextSections[y].classList.add('showCardsText');
+		y++;
+		if (y == 2) {
+			offerSectionOffsetMediumAndUnusual += 500;
+		}
+	} else if (
+		window.scrollY >= offerSectionOffsetMediumAndUnusual &&
+		window.innerWidth >= 1000 &&
+		window.innerWidth < 1200
+	) {
+		cardsTextSections[y].classList.add('showCardsText');
+		y++;
+		if (y == 2) {
+			offerSectionOffsetMediumAndUnusual += 550;
 		}
 	}
+
 	// JEŚLI SZEROKOŚĆ EKRANU PRZEKRACZA 1000 PX ALE JEST MNIEJSZA NIŻ 1200 PX TRAKTUJE TO JAKO ROZDZIELCZOŚĆ NIESTANDARDOWĄ (UNUSUAL)
 };
 
 setTimeout(showAndHidePhotos, 5000);
 
 //--------------EVENTS----------------
-document.addEventListener("DOMContentLoaded", checkScreenWidth);
-window.addEventListener("resize", checkScreenWidth);
-bBtn.addEventListener("click", handleNav);
-document.addEventListener("scroll", showCardsDescription);
-document.addEventListener("DOMContentLoaded", showCardsDescription);
+document.addEventListener('DOMContentLoaded', checkScreenWidth);
+window.addEventListener('resize', checkScreenWidth);
+bBtn.addEventListener('click', handleNav);
+document.addEventListener('scroll', showCardsDescription);
+document.addEventListener('DOMContentLoaded', showCardsDescription);
 
-navSpace.addEventListener("focusout", () => {
+navSpace.addEventListener('focusout', () => {
 	//Kacperek zabłysnął
-	document.body.classList.toggle("mobileMenuLock");
-	mbNav.classList.remove("mobile-nav--active");
+	document.body.classList.toggle('mobileMenuLock');
+	mbNav.classList.remove('mobile-nav--active');
 	offsetTimeout();
 	//to zrobił dominik elegancko nie
 });
