9333b4b21f465e7b74278c177673ce290d75d9f7
Kacper <krzysztofzajac975@gmail.com>
2024-03-09 17:19:19 +0100

    small;changes in form

--- html/form.php
+++ html/form.php
@@ -5,25 +5,26 @@
             $phone = $_POST['phone'];
             $subject = $_POST['subject'];
             $textarea = $_POST['message'];
+            $zipcode = $_POST['postcode'];
 
-            $message = "Imie i Nazwisko:".$name."\n"."E-mail:".$email."\n"."Numer telefonu:".$phone."\n".$textarea;
+            $message = "Imie i Nazwisko:".$name."\n"."E-mail:".$email."\n"."Numer telefonu:".$phone."\n"."Kod pocztowy".$zipcode."\n".$textarea;
 
             if(mail('domkacmijzajpost@domkacmijzaj.fun', 'Nowa wiadomość dla NORDWIT !', "$message")){
                 echo"<!DOCTYPE html>
-                <html lang="en">
+                <html lang='en'>
                 
                 <head>
-                    <meta charset="UTF-8">
-                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <meta charset='UTF-8'>
+                    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
                     <title>Document</title>
-                    <link rel="stylesheet" href="../css/main.css">
+                    <link rel='stylesheet' href='../css/main.css'>
                 </head>
                 
                 <body>
-                    <div class="form__afterpage">
+                    <div class='form__afterpage'>
                         <div>
                             <h1>Thank you for your message! We will answer soon.</h1>
-                            <a href="../html/index.html">
+                            <a href='../html/index.html'>
                                 <p>Back to main page</p>
                             </a>
                         </div>
@@ -31,31 +32,31 @@
                 </body>
                 
                 </html>";
+            }
                 else{
                     echo"<!DOCTYPE html>
-                    <html lang="en">
+                    <html lang='en'>
                     
                     <head>
-                        <meta charset="UTF-8">
-                        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                        <meta charset='UTF-8'>
+                        <meta name='viewport' content='width=device-width, initial-scale=1.0'>
                         <title>Document</title>
-                        <link rel="stylesheet" href="../css/main.css">
+                        <link rel='stylesheet' href='../css/main.css'>
                     </head>
                     
                     <body>
-                        <div class="form__afterpage">
+                        <div class='form__afterpage'>
                             <div>
                                 <h1>Something went wrong. Please try again later...</h1>
-                                <a href="../html/index.html">
+                                <a href='../html/index.html'>
                                     <p>Back to main page</p>
                                 </a>
                             </div>
                         </div>
                     </body>
                     
-                    </html>"
+                    </html>";
                 }
-            }
         }
 ?>
 
